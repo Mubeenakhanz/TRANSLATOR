acafae44aadf72cbbefe1194bafd61eadc5d13f2
nada <nadaessam94@gmail.com>
2019-11-18 19:46:08 -0700

    buttons added

--- Prototype0/app/src/main/java/com/cs515/prototype0/MainActivity.java
+++ Prototype0/app/src/main/java/com/cs515/prototype0/MainActivity.java
@@ -2,16 +2,19 @@ package com.cs515.prototype0;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
+//import com.google.android.material.floatingactionbutton.FloatingActionButton;
+//import com.google.android.material.snackbar.Snackbar;
+//import com.google.android.material.tabs.TabLayout;
+//import com.google.android.material.button.MaterialButton;
 
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.view.View;
 
 import com.cs515.prototype0.ui.main.SectionsPagerAdapter;
 
@@ -22,18 +25,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+//        ConstraintLayout viewPager = findViewById(R.id.view_pager);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        viewPager.setAdapter(sectionsPagerAdapter);
+
     }
 }
